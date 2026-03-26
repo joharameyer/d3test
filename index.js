f8e76eb884f7dc677ccccaa6f3b9419cfacadea6
joharameyer <83025393+joharameyer@users.noreply.github.com>
2026-03-26 09:32:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,61 @@
-// For bundlers such as Vite and Webpack omit https://esm.sh/
+// For bundlers like Vite/Webpack, remove esm.sh URLs if installed via npm
 import { select } from 'https://esm.sh/d3-selection';
 import { geoPath, geoMercator } from 'https://esm.sh/d3-geo';
 import { json } from 'https://esm.sh/d3-fetch';
 
-let projection = geoMercator()
+const svg = select('#content-svg');
+let projection = geoMercator();
+let geoGenerator = geoPath().projection(projection);
 
-let geoGenerator = geoPath()
-	.projection(projection);
-
+// Mouseover handler
 function handleMouseover(e, d) {
-    let pixelArea = geoGenerator.area(d);
-    let bounds = geoGenerator.bounds(d);
-    let centroid = geoGenerator.centroid(d);
-    let measure = geoGenerator.measure(d);
+  const pixelArea = geoGenerator.area(d);
+  const bounds = geoGenerator.bounds(d);
+  const centroid = geoGenerator.centroid(d);
+  const measure = geoGenerator.measure(d);
 
-    select('#content .info')
-        .text(d.properties["Constituency Name"] + 
-              ' (area = ' + pixelArea.toFixed(1) + 
-              ', measure = ' + measure.toFixed(1) + ')');
+  // Update info
+  select('#content .info')
+    .text(d.properties["Constituency Name"] +
+          ' (area = ' + pixelArea.toFixed(1) +
+          ', measure = ' + measure.toFixed(1) + ')');
 
-    select('#content .bounding-box rect')
-        .attr('x', bounds[0][0])
-        .attr('y', bounds[0][1])
-        .attr('width', bounds[1][0] - bounds[0][0])
-        .attr('height', bounds[1][1] - bounds[0][1]);
+  // Update bounding box
+  select('#content .bounding-box rect')
+    .attr('x', bounds[0][0])
+    .attr('y', bounds[0][1])
+    .attr('width', bounds[1][0] - bounds[0][0])
+    .attr('height', bounds[1][1] - bounds[0][1]);
 
-    select('#content .centroid')
-        .style('display', 'inline')
-        .attr('transform', 'translate(' + centroid + ')');
+  // Update centroid
+  select('#content .centroid')
+    .style('display', 'inline')
+    .attr('transform', 'translate(' + centroid + ')');
 }
 
+// Update map
 function update(geojson) {
-	let u = select('#content g.map')
-		.selectAll('path')
-		.data(geojson.features);
+  // Fit projection to SVG size
+  projection.fitSize([parseInt(svg.attr('width')), parseInt(svg.attr('height'))], geojson);
+  geoGenerator = geoPath().projection(projection);
 
-u.enter()
-  .append('path')
-  .attr('d', geoGenerator)       // only once!
-  .attr('fill', 'red')           // make it visible
-  .attr('stroke', 'black')
-  .attr('stroke-width', 0.5)
-  .on('mouseover', handleMouseover);
-	
+  const u = svg.select('g.map')
+    .selectAll('path')
+    .data(geojson.features);
+
+  u.enter()
+    .append('path')
+    .attr('d', geoGenerator)
+    .attr('fill', 'steelblue') // visible fill
+    .attr('stroke', '#fff')
+    .attr('stroke-width', 0.5)
+    .on('mouseover', handleMouseover);
 }
 
-
-
-// REQUEST DATA
-json('https://github.com/joharameyer/d3test/blob/487580a752f694b987040b539955d5959e01ed9f/ward.geojson')
-  .then(function(data) {
-    projection.fitSize([400, 600], data);
+// Load GeoJSON
+json('https://raw.githubusercontent.com/joharameyer/d3test/main/wards.geojson')
+  .then(data => {
+    console.log('Loaded features:', data.features.length);
     update(data);
-  });
-
+  })
+  .catch(err => console.error(err));
