afb4c674a648363d3c0a7132baf96020cf1aae43
joharameyer <83025393+joharameyer@users.noreply.github.com>
2026-03-26 09:41:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
-// For bundlers like Vite/Webpack, remove esm.sh URLs if installed via npm
 import { select } from 'https://esm.sh/d3-selection';
 import { geoPath, geoMercator } from 'https://esm.sh/d3-geo';
 import { json } from 'https://esm.sh/d3-fetch';
 
 const svg = select('#content-svg');
+const width = parseInt(svg.attr('width'));
+const height = parseInt(svg.attr('height'));
+
 let projection = geoMercator();
 let geoGenerator = geoPath().projection(projection);
 
-// Mouseover handler
+// Mouseover
 function handleMouseover(e, d) {
-  const pixelArea = geoGenerator.area(d);
   const bounds = geoGenerator.bounds(d);
   const centroid = geoGenerator.centroid(d);
-  const measure = geoGenerator.measure(d);
 
-  // Update info
   select('#content .info')
-    .text(d.properties["Constituency Name"] +
-          ' (area = ' + pixelArea.toFixed(1) +
-          ', measure = ' + measure.toFixed(1) + ')');
+    .text(d.properties["Constituency Name"]);
 
-  // Update bounding box
   select('#content .bounding-box rect')
     .attr('x', bounds[0][0])
     .attr('y', bounds[0][1])
     .attr('width', bounds[1][0] - bounds[0][0])
     .attr('height', bounds[1][1] - bounds[0][1]);
 
-  // Update centroid
   select('#content .centroid')
     .style('display', 'inline')
     .attr('transform', 'translate(' + centroid + ')');
@@ -35,18 +30,17 @@ function handleMouseover(e, d) {
 
 // Update map
 function update(geojson) {
-  // Fit projection to SVG size
-  projection.fitSize([parseInt(svg.attr('width')), parseInt(svg.attr('height'))], geojson);
+  // Fit projection to SVG
+  projection.fitSize([width, height], geojson);
   geoGenerator = geoPath().projection(projection);
 
-  const u = svg.select('g.map')
+  svg.select('g.map')
     .selectAll('path')
-    .data(geojson.features);
-
-  u.enter()
+    .data(geojson.features)
+    .enter()
     .append('path')
     .attr('d', geoGenerator)
-    .attr('fill', 'steelblue') // visible fill
+    .attr('fill', 'steelblue')
     .attr('stroke', '#fff')
     .attr('stroke-width', 0.5)
     .on('mouseover', handleMouseover);
@@ -58,4 +52,4 @@ json('https://raw.githubusercontent.com/joharameyer/d3test/f8e76eb884f7dc677cccc
     console.log('Loaded features:', data.features.length);
     update(data);
   })
-  .catch(err => console.error(err));
+  .catch(err => console.error('Error loading GeoJSON:', err));
