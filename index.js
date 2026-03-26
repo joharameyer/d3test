94e7a0185354fff5bd7a7f4781344c988c099379
joharameyer <83025393+joharameyer@users.noreply.github.com>
2026-03-26 08:42:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
 // For bundlers such as Vite and Webpack omit https://esm.sh/
-import { select, selectAll } from 'https://esm.sh/d3-selection';
+import { select } from 'https://esm.sh/d3-selection';
+import { geoPath, geoMercator } from 'https://esm.sh/d3-geo';
+import { json } from 'https://esm.sh/d3-fetch';
 
-// Change colour of first circle
-select('circle')
-	.style('fill', 'orange')
+let projection = geoMercator()
+	.scale(400)
+	.translate([200, 280])
+	.center([0, 5]);
 
-// Change radius of all circles
-selectAll('circle')
-	.attr('r', 20);
+let geoGenerator = geoPath()
+	.projection(projection);
 
-// Add .selected class to 3rd circle
-select('circle:nth-child(3)')
-	.classed('selected', true);
+function handleMouseover(e, d) {
+	let pixelArea = geoGenerator.area(d);
+	let bounds = geoGenerator.bounds(d);
+	let centroid = geoGenerator.centroid(d);
+	let measure = geoGenerator.measure(d);
 
-// Set checked property of checkbox
-select('input.robot-checkbox')
-	.property('checked', true);
+	select('#content .info')
+		.text(d.properties.name + ' (path.area = ' + pixelArea.toFixed(1) + ' path.measure = ' + measure.toFixed(1) + ')');
 
-// Set text on .title element
-select('.title')
-	.text('D3 in Depth selection example')
-	
+	select('#content .bounding-box rect')
+		.attr('x', bounds[0][0])
+		.attr('y', bounds[0][1])
+		.attr('width', bounds[1][0] - bounds[0][0])
+		.attr('height', bounds[1][1] - bounds[0][1]);
+
+	select('#content .centroid')
+		.style('display', 'inline')
+		.attr('transform', 'translate(' + centroid + ')');
+}
+
+function update(geojson) {
+	let u = select('#content g.map')
+		.selectAll('path')
+		.data(geojson.features);
+
+	u.enter()
+		.append('path')
+		.attr('d', geoGenerator)
+		.on('mouseover', handleMouseover);
+}
+
+
+
+// REQUEST DATA
+json('https://assets.codepen.io/2814973/africa.json')
+	.then(function(json) {
+		update(json)
+	});
+
+
